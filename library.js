ba856b33b9269e913665b93378d826b3d77dba68
Tom <thomasjh@gmail.com>
2024-12-10 15:20:41 +0100

    Adding logic, front-end styling. Not stored in local data yet.

--- library.js
+++ library.js
@@ -2,7 +2,8 @@
 const myLibrary = [];
 
 //Constructor for Book Object
-function Book(title, author, genre, read, description){
+function Book(title, author, genre, read, description, id){
+    this.id = id;
     this.title = title;
     this.author = author;
     this.genre = genre;
@@ -47,32 +48,75 @@ listLink.addEventListener('click', () => {
 
 //Handling Form Submission of Books and Reset
 let submitButton = document.getElementById("submit");
-submitButton.addEventListener('click', submitBook);
+submitButton.addEventListener('click', (event) => {
+    let form = document.forms.form;
+    if (!form.checkValidity()){
+        form.reportValidity()
+    }
+    else{
+        event.preventDefault();
+        submitBook();
+    };})
 
 // let resetButton = document.getElementById("reset");
 // resetButton.addEventListener('click', resetForm);
 
 function resetForm(event){
-    event.preventDefault();
+    
     document.forms.form.reset()
 }
 
 function submitBook(event){
-    event.preventDefault();
+    
+    var id = myLibrary.length
     var formEl = document.forms.form;
     var formData = new FormData(formEl);
-    var title = formData.get('title')
-    var author = formData.get('author')
-    var genre = formData.get('genre')
-    var read = formData.get('readunread')
-    var description = formData.get('description')
-    let new_book = new Book(title, author, genre, read, description)
-    document.getElementById("form").reset()
-    console.log(myLibrary)
-    myLibrary.push(new_book)
-    console.log(myLibrary)
+    var title = formData.get('title');
+    var author = formData.get('author');
+    var genre = formData.get('genre');
+    var read = formData.get('readunread');
+    console.log(read)
+    if (read == null){
+        read = "Unread";
+    }
+    var description = formData.get('description');
+    description = description.trim();
+    if(description == ""){
+        description = "No description Provided";
+    }
+    console.log(description)
+    let new_book = new Book(title, author, genre, read, description, id);
+    document.getElementById("form").reset();
+    console.log(new_book)
+    
+    myLibrary.push(new_book);
+    addToRecent(new_book);
+    
+}
 
+function addToRecent(new_book){
+    console.log(new_book);
+    let container = document.getElementById("recentlyAdded");
+    let bookDiv = document.createElement("div");
+    bookDiv.className = "book";
+    bookDiv.innerHTML = `<p style="font-weight: bold; text-align: center; margin-bottom: 0px; font-size: 1em;">${new_book.title}</p><hr><p style="font-size: 0.9em;">Author: ${new_book.author}</p><p style="font-size: 0.9em;">Genre: ${new_book.genre}</p><p data-id="${new_book.id}" class="desc" style="font-size: 0.9em;">Click for Description</p><p data-id="${new_book.id}" class="bookStatus" title="Click to change" style="font-size: 0.9em;">Read Status: ${new_book.read}</p>`;
+    container.appendChild(bookDiv);
+    let describer = bookDiv.querySelector(".desc");
+    describer.addEventListener('click', () => openDescribeModal(new_book));
+    
+}
 
+function openDescribeModal(book){
+    if (document.querySelector(".descriptionModal")){
+        document.querySelector(".descriptionModal").remove()
+    }
+    let modal = document.createElement("div");
+    modal.className = "descriptionModal";
+    modal.innerHTML = `<h2>${book.title}</h2><p style="text-align: center;">${book.description}</p><p style="text-align: right" onclick="closeDescribeModal()">Close Modal</p>`;
+    document.body.appendChild(modal);
+}
 
-    event.preventDefault();
+function closeDescribeModal(){
+    let modal = document.querySelector(".descriptionModal");
+    modal.remove();
 }
